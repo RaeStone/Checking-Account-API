0c329df17e881d7d875cc151c499ce8a59dab107
Rae <RaeTStone@gmail.com>
2022-01-19 16:12:49 -0500

    added error handling through try catch

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,55 +5,94 @@ const Accounts = db.Accounts;
 
 //admin methods
 const addAccount = async (req, res) => {
-    let input_data = {
-        currentAmount: req.body.currentAmount,
-        accountNumber: req.body.accountNumber,
-        routingNumber: req.body.routingNumber,
-        userId: req.body.userId
+    try {
+        let input_data = {
+            currentAmount: req.body.currentAmount,
+            accountNumber: req.body.accountNumber,
+            routingNumber: req.body.routingNumber,
+            userId: req.body.userId
+        }
+    
+        const account = await Accounts.create(input_data);
+        res.status(200).send(account);
     }
-
-    const account = await Accounts.create(input_data);
-    res.status(200).send(account);
+    catch(error) {
+        res.status(400).send(error);
+    }
+    
 }
 
 const deleteAccount = async (req, res) => {
-    let id = req.params.id;
+    try {
+        let id = req.params.id;
 
-    await Accounts.destroy({where :{id: id}});
-    res.status(200).send(`account with id: ${id} is deleted`);
+        await Accounts.destroy({where :{id: id}});
+        res.status(200).send(`account with id: ${id} is deleted`);
+    }
+    catch(error) {
+        res.status(400).send(error);
+    }
+    
 }
 
 const updateAccountTotal = async (req, res) => {
-    let id = req.params.id;
-    let total = req.body.total;
+    try {
+        let id = req.params.id;
+        let total = req.body.total;
 
-    await Accounts.update({currentAmount: total}, {where :{id: id}});
-    res.status(200).send(`account with id: ${id} has new total: ${total}`);
+        await Accounts.update({currentAmount: total}, {where :{id: id}});
+        res.status(200).send(`account with id: ${id} has new total: ${total}`);
+    }
+    catch(error) {
+        res.status(400).send(error);
+    }
+    
 }
 
 //common methods
 const getAllAccounts = async (req, res) => {
-    let accounts = await Accounts.findAll({});
-    res.status(200).send(accounts);
+    try {
+        let accounts = await Accounts.findAll({});
+        res.status(200).send(accounts);
+    }
+    catch(error) {
+        res.status(400).send(error);
+    }
 }
 
 const getOneAccount = async (req, res) => {
-    let id = req.params.id;
-
-    let account = await Accounts.findOne({where: {id : id}});
-    res.status(200).send(account);
+    try {
+        let id = req.params.id;
+        let account = await Accounts.findOne({where: {id : id}});
+        res.status(200).send(account);
+    }
+    catch(error) {
+        res.status(400).send(error);
+    }
 }
 
-const getAccountFull = async (req, res) => {
-    let id = req.params.id;
 
-    let account = await Accounts.findOne({where: {id : id}, include: db.Transactions});
-    res.status(200).send(account);
+
+const getAccountFull = async (req, res) => {
+    try {
+        let id = req.params.id;
+    
+        let account = await Accounts.findOne({where: {id : id}, include: db.Transactions});
+        res.status(200).send(account);
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
 }
 
 const getAllAccountsFull = async (req, res) => {
-    let accounts = await Accounts.findAll({include: db.Transactions});
-    res.status(200).send(accounts);
+    try {
+        let accounts = await Accounts.findAll({include: db.Transactions});
+        res.status(200).send(accounts);
+    }
+    catch(error) {
+        res.status(400).send(error);
+    }
 }
 
 module.exports = {
--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,43 +4,68 @@ const Transactions = db.Transactions;
 
 //admin methods
 const addTransaction = async (req, res) => {
-    let input_data = {
-        type: req.body.type,
-        amount: req.body.amount,
-        date: req.body.date,
-        accountId: req.body.accountId
+    try {
+        let input_data = {
+            type: req.body.type,
+            amount: req.body.amount,
+            date: req.body.date,
+            accountId: req.body.accountId
+        }
+        console.log(input_data);
+    
+        const transaction = await Transactions.create(input_data);
+        res.status(200).send(transaction)
     }
-    console.log(input_data);
-
-    const transaction = await Transactions.create(input_data);
-    res.status(200).send(transaction);
+    catch(error) {
+        res.status(400).send(error);
+    }
 }
 
 const deleteTransaction = async (req, res) => {
-    let id = req.params.id;
-
-    await Transactions.destroy({where :{id: id}});
-    res.status(200).send(`Transaction with id: ${id} is deleted`);
+    try {
+        let id = req.params.id;
+    
+        await Transactions.destroy({where :{id: id}});
+        res.status(200).send(`Transaction with id: ${id} is deleted`);
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
 }
 
 //common methods
 const getAllTransactions = async (req, res) => {
-    let transactions = await Transactions.findAll({});
-    res.status(200).send(transactions);
+    try {
+        let transactions = await Transactions.findAll({});
+        res.status(200).send(transactions);
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
 }
 
 const getOneTransaction = async (req, res) => {
-    let id = req.params.id;
-
-    let transactions = await Transactions.findOne({where: {id : id}});
-    res.status(200).send(transaction);
+    try {
+        let id = req.params.id;
+    
+        let transactions = await Transactions.findOne({where: {id : id}});
+        res.status(200).send(transaction);
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
 }
 
 const getAccountTransactions = async (req, res) => {
-    let accountId = req.params.id;
-
-    let transactions = await Transactions.findAll({where: {accountId : accountId}});
-    res.status(200).send(transactions);
+    try {
+        let accountId = req.params.id;
+    
+        let transactions = await Transactions.findAll({where: {accountId : accountId}});
+        res.status(200).send(transactions);
+    }
+    catch(error) {
+        res.status(400).send(error);
+    }
 }
 
 module.exports = {
