8c5bd11fe1fee5b575d7c956c7ae47664e137fe4
Rae <RaeTStone@gmail.com>
2022-02-08 11:03:46 -0500

    changed date to date type

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false
         },
         date: {
-            type: DataTypes.STRING,
+            type: DataTypes.DATE,
             allowNull: false
         },
         accountId: {
